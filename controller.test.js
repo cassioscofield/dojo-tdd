ff573d60790e61110397212f62b2798fb4f6f088
Cassio.Scofield <cassio.scofield@pebmed.com.br>
2022-03-23 08:00:48 -0300

    Parte 2 - 200 e 404

--- controller.test.js
+++ controller.test.js
@@ -41,3 +41,66 @@ it('Deve retornar 401 se o token de autorização não for enviado no header', d
     // Act
 	controller()(req, res);
 });
+
+it('Deve retornar 404 se a campanha não for encontrada', done => {
+	// Arrange
+	req = {
+        params: {
+            idCampanha: '1'
+        },
+        headers: {
+            'Authorization': 'x'
+        }
+    };
+	res = {
+		status: function(statusCode) {
+            // Assert
+            expect(statusCode).toBe(404);
+		},
+		send: function(body) {
+			expect(body.mensagem).toBeTruthy();
+			done();
+		}
+	}
+    const campanhaRepositoryMock = {
+        findById: function (id){
+            if (id == 2) {
+                return true;
+            }
+            return false;
+        }
+    };
+    // Act
+	controller(campanhaRepositoryMock)(req, res);
+});
+
+it('Deve retornar 200 se a campanha for encontrada', done => {
+	// Arrange
+	req = {
+        params: {
+            idCampanha: '2'
+        },
+        headers: {
+            'Authorization': 'x'
+        }
+    };
+	res = {
+		status: function(statusCode) {
+            // Assert
+            expect(statusCode).toBe(200);
+		},
+		send: function(body) {
+			done();
+		}
+	}
+    const campanhaRepositoryMock = {
+        findById: function (id){
+            if (id == 2) {
+                return true;
+            }
+            return false;
+        }
+    };
+    // Act
+	controller(campanhaRepositoryMock)(req, res);
+});
