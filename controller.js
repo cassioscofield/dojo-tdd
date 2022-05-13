b9333e7f35358ae717aaa6aa68b66f55ce38bede
Cassio.Scofield <cassio.scofield@pebmed.com.br>
2022-05-13 18:19:34 -0300

    Exercicio turma a

--- controller.js
+++ controller.js
@@ -1,8 +1,12 @@
 const MENSAGEM_ID_CAMPANHA_INVALIDO = 'idCampanha inválido';
 const MENSAGEM_AUTHORIZATION_NAO_ENVIADO = 'authorization não foi enviado no header';
+const MENSAGEM_CAMPANHA_NAO_ENCONTRADA = 'Campanha não encontrada';
+const MENSAGEM_CAMPANHA_ENCONTRADA_COM_SUCESSO = 'Campanha encontrada sucesso';
 
 const STATUS_CODE_BAD_REQUEST = 400;
 const STATUS_CODE_UNAUTHORIZED = 401;
+const STATUS_CODE_NOT_FOUND = 404;
+const STATUS_CODE_FOUND = 200;
 
 module.exports = controller = function(campanhaRepository) {
     return function (req, res) {
@@ -11,8 +15,25 @@ module.exports = controller = function(campanhaRepository) {
             res.send({mensagem: MENSAGEM_AUTHORIZATION_NAO_ENVIADO});
             return;
         }
-        res.status(STATUS_CODE_BAD_REQUEST);
-        res.send({mensagem: MENSAGEM_ID_CAMPANHA_INVALIDO});
+
+        if(isNaN(req.params.idCampanha)){
+            res.status(STATUS_CODE_BAD_REQUEST);
+            res.send({mensagem: MENSAGEM_ID_CAMPANHA_INVALIDO});
+            return;
+        }
+
+        const campanha = campanhaRepository.find(req.params.idCampanha);
+
+        if (campanha === 'undefined') {
+            res.status(STATUS_CODE_NOT_FOUND);
+            res.send({mensagem: MENSAGEM_CAMPANHA_NAO_ENCONTRADA});
+            return;
+        }
+        
+        res.status(STATUS_CODE_FOUND);
+        res.send({ mensagem: MENSAGEM_CAMPANHA_ENCONTRADA_COM_SUCESSO, data: campanha });
         return;
+        
+        
     };
 }
--- controller.test.js
+++ controller.test.js
@@ -1,4 +1,5 @@
 const controller = require('./controller');
+
 it('Deve retornar 400 se o usuário passar um idCampanha não for numérico', done => {
 	// Arrange
 	const req = {
@@ -41,3 +42,159 @@ it('Deve retornar 401 se o token de autorização não for enviado no header', d
     // Act
 	controller()(req, res);
 });
+
+it('Deve retornar 404 se a campanha não for encontrada', done => {
+	// Arrange
+	const idCampanha = '3334';
+	
+	const repository = {
+		find: (id) => {
+			if(id === '333') {
+				return {
+					id: 333,
+					nome: 'dojo',
+					dataExpiracao: 'data',
+				}
+			}
+			return 'undefined';
+		}
+	}
+	req = {
+        headers: { 'Authorization': 'x' },
+        params: {
+			idCampanha: idCampanha
+		},
+    };
+	res = {
+		status: function(statusCode) {
+            // Assert
+            expect(statusCode).toBe(404);
+		},
+		send: function(body) {
+			expect(body.mensagem).toBeTruthy();
+			done();
+		}
+	}
+    // Act
+	controller(repository)(req, res);
+});
+
+it('Deve retornar 200 em caso de campanha encontrada', done => {
+	// Arrange
+	const repository = {
+		find: (id) => {
+			return {
+				id: id,
+				nome: 'dojo',
+				dataExpiracao: 'data',
+			}
+		}
+	}
+
+	req = {
+        headers: { 'Authorization': 'x' },
+        params: {
+			idCampanha: '999'
+		},
+    };
+	res = {
+		status: function(statusCode) {
+            // Assert
+            expect(statusCode).toBe(200);
+		},
+		send: function(body) {
+			expect(body.mensagem).toBeTruthy();	
+			done();
+		}
+	}
+    // Act
+	controller(repository)(req, res);
+});
+
+it('Deve retornar 200 e os dados da campanha', done => {
+	// Arrange
+	const idCampanha = '3334'
+	const expectedResult = {
+		id: idCampanha,
+		nome: 'dojo',
+		dataExpiracao: 'data',
+	}
+
+	const repository = {
+		find: () => {
+			return {
+				id: idCampanha,
+				nome: 'dojo',
+				dataExpiracao: 'data',
+			}
+		}
+	}
+
+	req = {
+		headers: { 'Authorization': 'x' },
+		params: {
+			idCampanha,
+		},
+	};
+
+	res = {
+		status: function (statusCode) {
+			// Assert
+			expect(statusCode).toBe(200);
+		},
+
+		send: function (body) {
+			expect(body.mensagem).toBeTruthy();
+			expect(body).toHaveProperty('data', expectedResult);
+			expect(body.data).toHaveProperty('id', idCampanha);
+			done();
+		}
+	}
+	// Act
+	controller(repository)(req, res);
+});
+
+it('Deve retornar 200 e o controller deve chamar o repository com o id de campanha', done => {
+	// Arrange
+	const idCampanha = '3334'
+	const expectedResult = {
+		id: idCampanha,
+		nome: 'dojo',
+		dataExpiracao: 'data',
+	}
+
+
+	const repository = {
+		find: (id) => {
+			expect(id).toBe(idCampanha);
+			return {
+				id: id,
+				nome: 'dojo',
+				dataExpiracao: 'data',
+			}
+		}
+	}
+
+	req = {
+		headers: { 'Authorization': 'x' },
+		params: {
+			idCampanha,
+		},
+	};
+
+	res = {
+		status: function (statusCode) {
+			// Assert
+			expect(statusCode).toBe(200);
+		},
+
+		send: function (body) {
+			expect(body.mensagem).toBeTruthy();
+			expect(body).toHaveProperty('data', expectedResult);
+			expect(body.data).toHaveProperty('id', idCampanha);
+			done();
+		}
+	}
+	// Act
+	controller(repository)(req, res);
+})
